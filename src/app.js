f6fbc0ee91e21b3110e7dddfc1a8d940d7674d12
sourabhkt1997 <sourabh.dhanarajan@gmail.com>
2024-03-19 16:08:32 +0530

    completed

--- src/app.js
+++ src/app.js
@@ -2,7 +2,15 @@
 // unless absolutely needed.
 
 //Initial price of the burger
-var wholeWheatBun = 10;
+
+let price = {
+  wholeWheatBun :10,
+  Patty: 80,
+  Cheese: 10,
+  Tomatoes:20,
+  Onions: 20,
+  Lettuce: 20
+};
 
 //Ingredients of the burger along with the price
 // Clue: the name is same as the textcontent of the button. Will be useful later on :)
@@ -47,18 +55,43 @@ function renderPatty() {
 
 function renderCheese() {
   //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
+  let cheese = document.querySelector("#cheese")
+  if (state.Cheese) {
+    cheese.style.display = "inherit";
+  } else {
+    cheese.style.display = "none";
+  }
 }
 
 function renderTomatoes() {
   //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
+  let tomato = document.querySelector("#tomato")
+  if (state.Tomatoes) {
+    tomato.style.display = "inherit";
+  } else {
+    tomato.style.display = "none";
+  }
 }
 
 function renderOnions() {
   //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
+  let onion = document.querySelector("#onion")
+  if (state.Onions) {
+    onion.style.display = "inherit";
+  } else {
+    onion.style.display = "none";
+  }
 }
 
 function renderLettuce() {
   //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
+  let lettuce = document.querySelector("#lettuce")
+  if (state.Lettuce) {
+    lettuce.style.display = "inherit";
+  } else {
+    lettuce.style.display = "none";
+
+  }
 }
 
 document.querySelector(".btn-patty").onclick = function () {
@@ -67,23 +100,94 @@ document.querySelector(".btn-patty").onclick = function () {
 };
 
 // Trial 2 - Setup event listener for the cheese button
+document.querySelector(".btn-cheese").onclick = function () {
+  state.Cheese = !state.Cheese;
+  renderAll();
+};
 
 
 // Trial 2 - Setup event listener for the tomatoes button
-
+document.querySelector(".btn-tomatoes").onclick = function () {
+  state.Tomatoes = !state.Tomatoes;
+  renderAll();
+};
 
 // Trial 2 - Setup event listener for the onion button
+document.querySelector(".btn-onions").onclick = function () {
+  state.Onions = !state.Onions;
+  renderAll();
+};
 
 
 // Trial 2 - Setup event listener for the lettuce button
+document.querySelector(".btn-lettuce").onclick = function () {
+  state.Lettuce = !state.Lettuce;
+  renderAll();
+};
 
 
 //Challenge 1 - Add/Remove the class active to the buttons based on state
 
+function  renderButtons(){
+  let btns = document.querySelectorAll(".button")
+ 
+btns.forEach(element => {
+  if(!state[element.textContent]){
+    element.classList.remove("active");
+  }
+  else{
+    element.classList.add("active");
+  }
+});
 
-//Challenge 2 - Render only the items selected in the ingredients board based on the state
+
+}
+
+//hallenge 2 - Render only the items selected in the ingredients board based on the state
+
+function renderIngredientsBoard(){
+
+  let btns = document.querySelectorAll(".button")
+  let items = document.querySelectorAll(".items")
+  
+  console.log(btns)
+
+  btns.forEach(element => {
+    if(!element.classList.contains("active")){
+        items.forEach((ele)=>{
+          if(ele.textContent == element.textContent){
+            ele.style.display="none"
+          }
+        })
+     }
+     else{
+      items.forEach((ele)=>{
+        if(ele.textContent == element.textContent){
+          ele.style.display="block"
+        }
+      })
+     }
+  });
+
+
+}
 
 
 //Judgement 1
 //In the p element having price-details as the class, display the calculated
 //price based on ingredients
+
+function renderPrice(){
+  let btns = document.querySelectorAll(".button")
+   let toatalPrice=document.querySelector(".price-details")
+   
+   let total = 170
+  btns.forEach((element)=>{
+   if(!element.classList.contains("active")){
+      total=total-price[element.textContent]
+   }
+  })
+
+
+   toatalPrice.textContent=`INR ${total}`
+}
